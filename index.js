a5426f9e4ead1e74bd0c5942b1aefa07db4b7938
Iroda Yılmaz <irodaand@hotmail.com>
2023-03-18 13:00:28 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function countDown(num){
+    for(let i= num; i >= 0; i--)
+        console.log(i);
+}
+function writeCards(name){
+    let array = [];
+    for (let index = 0; index < name.length; index++) {
+        let temp = "Thank you, " + name[index] + ", for the wonderful surprise gift!";
+        array.push(temp);
+    }
+    return array;
+}
